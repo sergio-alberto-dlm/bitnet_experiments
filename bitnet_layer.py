6ab11b530e49e7b5be6b193c094329b8913fad11
Sergio <sergio.deleon@cimat.mx>
2024-05-14 17:58:52 -0600

    First experiment with BitLinear

---
+++ bitnet_layer.py
@@ -0,0 +1,69 @@
+from torch import nn, Tensor
+from torch.nn import LayerNorm
+import torch.nn.functional as F
+
+
+def activation_quant(x: Tensor):
+    """Per token quantization to 8bits. No grouping is needed for quantization
+
+    Args:
+        x (Tensor): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    scale = 127.0 / x.abs().max(dim=-1, keepdim=True).values.clamp_(min=1e-5)
+    y = (x * scale).round().clamp_(-128, 127) / scale
+    return y
+
+
+def weight_quant(w: Tensor):
+    scale = w.abs().mean()
+    e = w.mean()
+    u = (w - e).sign() * scale
+    return u
+
+
+class BitLinear(nn.Linear):
+    """
+    Custom linear layer with bit quantization.
+
+    Args:
+        dim (int): The input dimension of the layer.
+        training (bool, optional): Whether the layer is in training mode or not. Defaults to False.
+        *args: Variable length argument list.
+        **kwargs: Arbitrary keyword arguments.
+
+    Attributes:
+        dim (int): The input dimension of the layer.
+
+    """
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Forward pass of the BitLinear layer.
+
+        Args:
+            x (Tensor): The input tensor.
+
+        Returns:
+            Tensor: The output tensor.
+
+        """
+        w = self.weight
+        x_norm = LayerNorm(self.in_features)(x)
+
+        # STE using detach
+        x_quant = x_norm + (activation_quant(x_norm) - x_norm).detach()
+        w_quant = w + (weight_quant(w) - w).detach()
+        y = F.linear(x_quant, w_quant)
+        return y
+    
+
+
+
+
+
+
+
+# Eternal Reclaimer. (2023). BitNet. GitHub. https://github.com/kyegomez/BitNet?tab=MIT-1-ov-file (accessed May 14, 2024)
